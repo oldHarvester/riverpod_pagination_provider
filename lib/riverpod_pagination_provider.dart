8051d275b644f58950d9c7fa63a7ed9744bccae8
Daler <108223768+oldHarvester@users.noreply.github.com>
2025-10-13 14:20:26 +0500

    added exports

--- lib/riverpod_pagination_provider.dart
+++ lib/riverpod_pagination_provider.dart
@@ -1,2 +1,8 @@
-export 'package:riverpod_pagination_provider/src/models/pagination_params/pagination_params.dart';
 export 'package:riverpod_pagination_provider/src/provider/pagination_provider.dart';
+export 'package:riverpod_pagination_provider/src/models/pagination_update_type.dart';
+export 'package:riverpod_pagination_provider/src/models/pagination_state/pagination_state.dart';
+export 'package:riverpod_pagination_provider/src/models/pagination_relative_index/pagination_relative_index.dart';
+export 'package:riverpod_pagination_provider/src/models/pagination_params/pagination_params.dart';
+export 'package:riverpod_pagination_provider/src/models/pagination_page_state/pagination_page_state.dart';
+export 'package:riverpod_pagination_provider/src/models/pagination_page_response/pagination_page_response.dart';
+export 'package:riverpod_pagination_provider/src/models/paginated_list_response/paginated_list_response.dart';
--- lib/src/provider/pagination_provider.dart
+++ lib/src/provider/pagination_provider.dart
@@ -11,6 +11,7 @@ import 'package:flutter_toolkit/flutter_toolkit.dart';
 import '../models/pagination_page_response/pagination_page_response.dart';
 import '../models/pagination_params/pagination_params.dart';
 import '../models/pagination_relative_index/pagination_relative_index.dart';
+import '../models/pagination_update_type.dart';
 import '../utils/exceptions/pagination_cancel_exception.dart';
 
 part 'pagination_mixin.dart';
@@ -20,29 +21,4 @@ part 'notifiers/family_notifier.dart';
 part 'notifiers/autodispose_notifier.dart';
 part 'notifiers/notifier.dart';
 
-enum PaginationUpdateType {
-  /// При смене страницы старые страницы очищаются
-  clearOthers,
 
-  /// При смене страницы старые загруженные страницы не очищаются но обновляются
-  autoUpdateCache,
-
-  /// При смене страницы стырые загруженные страницы не очищаются и не обновляются
-  /// Исключением является присутствующая ошибка на странице
-  nonUpdateCache,
-}
-
-class ExamplePaginationNotifier
-    extends AutoDisposePaginationNotifier<List<int>, int> {
-  @override
-  int get initialLoadParams => 10;
-
-  @override
-  Future<PaginatedListResponse<List<int>>> fetchItems(
-    int loadParams,
-    Null arg,
-    PaginationParams paginationParams,
-  ) {
-    throw UnimplementedError();
-  }
-}
