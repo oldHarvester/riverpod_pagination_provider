b4b82111a73028b548cbb777841c2a000f4ca1bf
Daler <108223768+oldHarvester@users.noreply.github.com>
2025-10-15 10:47:27 +0500

    fix pagination params

--- lib/src/models/pagination_params/pagination_params.dart
+++ lib/src/models/pagination_params/pagination_params.dart
@@ -2,6 +2,11 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'pagination_params.freezed.dart';
 
+enum PaginationParamsQueryType {
+  limitOffset,
+  pageLimit,
+}
+
 @Freezed(fromJson: false, toJson: false)
 abstract class PaginationParams with _$PaginationParams {
   const PaginationParams._();
@@ -22,4 +27,19 @@ abstract class PaginationParams with _$PaginationParams {
   }) = _PaginationParams;
 
   int get page => (offset / limit).floor();
+
+  Map<String, dynamic> toQuery({
+    PaginationParamsQueryType type = PaginationParamsQueryType.limitOffset,
+  }) {
+    return switch (type) {
+      PaginationParamsQueryType.limitOffset => {
+        "limit": limit,
+        "offset": offset,
+      },
+      PaginationParamsQueryType.pageLimit => {
+        "page": page,
+        "limit": limit,
+      },
+    };
+  }
 }
